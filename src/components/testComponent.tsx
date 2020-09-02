8c55843a97ddcfbe89940c50f9b7451dde8a3d49
Ulric Todman <rick.sinclair.nyc@gmail.com>
2020-09-01 23:27:22 -0400

    added boostrap, typescript, icons, etc

---
+++ src/components/testComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const TestComponent = () => {
+  return (
+    <>
+      <img
+        src="https://cdn.stocksnap.io/img-thumbs/960w/valley-road_OM6BSRENPV.jpg"
+        alt="winding road"
+      />
+    </>
+  );
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Carousel } from "../components/Carousel/Carousel";
+export const Home = () => {
+  return (
+    <div>
+      <h1 className="display-3">Welcome to the Recon Mission</h1>
+
+      <Carousel />
+    </div>
+  );
+};
